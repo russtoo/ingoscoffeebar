22829280931931dfafe5bea57d70f924a6d3b394
musatrd <musartdube@gmail.com>
2023-03-31 00:50:51 +0200

    why us section wording

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,6 +70,19 @@
   }
 
   /**
+   * why us section fade in
+   */
+  window.addEventListener('scroll', function () {
+    const element = document.querySelector('.box');
+    const position = element.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3;
+
+    if (position < screenPosition) {
+      element.classList.add('active');
+    }
+  });
+
+  /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
   let selectHeader = select('#header')
